2c4cbca4c7f4b1427e79f10a4d4e0d8c31761aec
ilkinhuseynov1990 <49427513+ilkinhuseynov1990@users.noreply.github.com>
2019-06-09 23:50:28 +0400

    Add files via upload

---
+++ Layihe 05.06.2019/js/index.js
@@ -0,0 +1,215 @@
+$(document).ready(function () {
+
+    var positioncountertop = Math.floor($("#counter").position()) - 29;
+
+    
+    //scale image on load
+
+    $("#slider img").css({
+        "transform": "scale(1.1,1.1)",
+        "transition": "5s"
+    });
+
+    //box shadow onload
+
+    if (window.scrollY != 0) {
+        $("#header").css({
+            "box-shadow": "0 10px 20px #ccc"
+        })
+
+    } else {
+        $("#header").css({
+            "box-shadow": "0 0 0"
+        })
+    }
+
+    // slider show and hide
+
+    $(window).scroll(function () {
+
+        var top = 300;
+
+        var changelogo = $("#header").data("src");
+
+        var img = $("#header img");
+
+        if (window.scrollY == 0) {
+
+            $("#header").slideDown("1000");
+
+            img.attr("src", "img/logo-dark.png");
+
+            $("#header a").css({
+                "color": "#fff",
+
+            }); // buna bax link caralmalidir
+
+            $("#header").css({
+                "box-shadow": "0 0 0"
+            })
+
+
+        } else {
+
+            if (window.scrollY < top) {
+
+                $("#header").hide();
+
+                img.attr("src", `img/${changelogo}`);
+
+                $("#header a").css("color", "black");
+
+                $("#header").css({
+                    "box-shadow": "0 10px 20px #ccc"
+                })
+
+                $("#header").css({
+                    "position": "fixed",
+                    "width": "100%",
+                    "background-color": ""
+                })
+
+
+            } else {
+                $("#header").slideDown("fast");
+
+                img.attr("src", `img/${changelogo}`);
+
+                $("#header").css({
+                    "position": "fixed",
+                    "width": "100%",
+                    "background-color": "#fff"
+                })
+
+            }
+        }
+
+
+        // counter count begin;
+
+
+        if (positioncountertop == window.scrollY) {
+            number($(".number1"), 15);
+
+            number($(".number2"), 15);
+
+            numberthound($(".number"), 20);
+
+            numberthound($(".number3"), 10);
+        
+        }
+
+    })
+
+
+
+    function number(elm, interval) {
+
+        var datato = elm.data("to");
+        var datafrom = elm.data("from");
+
+        var inter = setInterval(function () {
+
+            if (datafrom < datato) {
+
+                datafrom += 1;
+
+                elm.text(datafrom);
+
+            } else {
+
+                clearInterval(inter);
+            }
+
+        }, interval)
+
+    }
+
+
+    function numberthound(elm, interval) {
+
+        var datato = elm.data("to");
+        var datafrom = elm.data("from");
+
+        var inter = setInterval(function () {
+
+            if (datafrom < datato) {
+
+                datafrom += 20;
+
+                elm.text(datafrom);
+
+            } else {
+
+                clearInterval(inter);
+            }
+
+        }, interval)
+
+    }
+
+
+
+    $("#left").click(function () {
+
+        var slide = $(".slider-content").children(".anim");
+
+        if (slide.next().length != 0) {
+
+            slide.animate({ "margin-right": "0" });
+
+            slide.animate({ "margin-left": "-=1140px" }, 2000);
+
+            slide.next().animate({ "margin-left": "0" }, 2000)
+
+            slide.next().addClass("anim");
+
+            slide.removeClass("anim");
+
+        }
+
+    })
+
+
+    $("#right").click(function () {
+
+        var slide = $(".slider-content").children(".anim");
+
+        if (slide.prev().length != 0) {
+
+
+            slide.animate({ "margin-right": "0" });
+
+            slide.animate({ "margin-left": "+=1140px" }, 2000);
+
+            slide.prev().animate({ "margin-left": "+=1140" }, 2000)
+
+            slide.prev().addClass("anim");
+
+            slide.removeClass("anim");
+        }
+
+    })
+
+
+
+    // toggle menu;
+
+    $(".button").click(function(){
+         
+        if(!($("#header ul.menu").hasClass("toggle"))){
+
+            
+            $("#header ul.menu").hide();
+            $("#header ul.menu").addClass("toggle");
+
+        }else{
+            $("#header ul.menu").show();
+            $("#header ul.menu").removeClass("toggle");
+        }
+
+    })
+
+
+
+})
---
+++ Layihe 05.06.2019/js/pluginminislider.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    // plugin start code
+
+    var jssor_1_SlideshowTransitions = [
+        { $Duration: 700, $Opacity: 2, $Brother: { $Duration: 700, $Opacity: 2 } }
+    ];
+
+    var jssor_1_options = {
+        $AutoPlay: 1,
+        $SlideEasing: $Jease$.$Linear,
+        $PauseOnHover: 0,
+        $SlideshowOptions: {
+            $Class: $JssorSlideshowRunner$,
+            $Transitions: jssor_1_SlideshowTransitions,
+            $TransitionsOrder: 1
+        },
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*#region responsive code begin*/
+
+    var MAX_WIDTH = 420;
+
+    function ScaleSlider() {
+        var containerElement = jssor_1_slider.$Elmt.parentNode;
+        var containerWidth = containerElement.clientWidth;
+
+        if (containerWidth) {
+
+            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+            jssor_1_slider.$ScaleWidth(expectedWidth);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+
+    ScaleSlider();
+
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    /*#endregion responsive code end*/
+
+})
